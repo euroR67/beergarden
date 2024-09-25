feead9435ea021910477c7d9b18bb1acd5932915
euroR67 <mchamaev67@gmail.com>
2024-09-25 16:11:36 +0200

    add js to toggle mobile menu

---
+++ src/script.js
@@ -0,0 +1,16 @@
+// Récupère les l'élément fa-bars et fa-times du DOM
+const bars = document.querySelector('.fa-bars');
+const times = document.querySelector('.fa-times');
+// Récupère l'élément nav avec l'id mobile-menu du DOM
+const mobileMenu = document.querySelector('#mobile-menu');
+
+// Fonction qui permet l'ouverture et fermeture du menu en faisant passer la classe de mobileMenu de left-full à left-0
+function toggleMenu() {
+    mobileMenu.classList.toggle('left-full');
+    mobileMenu.classList.toggle('left-0');
+}
+
+// Ajoute un écouteur d'événement sur l'élément bars qui déclenche la fonction toggleMenu
+bars.addEventListener('click', toggleMenu);
+// Ajoute un écouteur d'événement sur l'élément times qui déclenche la fonction toggleMenu
+times.addEventListener('click', toggleMenu);
